a3a1d770e1716aa95c6f2eb05dc0ab62bfa5200c
Sander Moonemans <sander.moonemans@outlook.com>
2023-05-26 15:50:57 +0200

    fixed file name paths in data_wrapper_test

--- src/cleaner/data_wrapper_test.py
+++ src/cleaner/data_wrapper_test.py
@@ -1,20 +1,20 @@
 from pathlib import Path
 from data_wrapper import DataWrapper, DataMatcher
 
-IMDB_top_250 = DataWrapper(Path('Consultant\Datasets\IMDB Top 250 Movies.csv'))
+IMDB_top_250 = DataWrapper(Path('data\IMDB Top 250 Movies.csv'))
 IMDB_top_250.set_headers('_', 'movie_name', 'movie_date', 'movie_rating', 'movie_genre', 'movie_censor', 'movie_runtime', 
                          'movie_overview', 'movie_budget', 'movie_box_office', 'actors', 'directors', 'writers')
 IMDB_all_genres = DataWrapper(Path('Consultant\Datasets\IMDb_All_Genres_etf_clean1.csv'))
 IMDB_all_genres.set_headers('movie_name', 'movie_date', 'director', 'actors', 'movie_rating', 'movie_runtime', 
                             'movie_censor', 'movie_gross', 'movie_genre', 'movie_side_genre')
-movies = DataWrapper(Path('Consultant\Datasets\movies.csv'))
+movies = DataWrapper(Path('data\movies.csv'))
 movies.set_headers('movie_name', 'movie_censor', 'movie_genre', 'movie_year', 'movie_date', 'movie_rating', 
                    'movie_rating_count', 'director', 'writer', 'actor', 'movie_country', 'movie_budget', 'movie_gross', 
                    'movie_company', 'movie_runtime')
-mymovies = DataWrapper(Path('Consultant\Datasets\mymoviedb.csv'))
+mymovies = DataWrapper(Path('data\mymoviedb.csv'))
 mymovies.set_headers('movie_date', 'movie_name', 'movie_overview', 'movie_popularity', 'movie_rating_count', 'movie_rating', 
                      'movie_language', 'movie_genre', 'movie_poster')
-oscar_demographics = DataWrapper(Path('Consultant\Datasets\Oscars-demographics-DFE.csv'))
+oscar_demographics = DataWrapper(Path('data\Oscars-demographics-DFE.csv'))
 oscar_demographics.set_headers('movie_id', 'award_winner', '_', 'movie_rating', 'movie_rating_time', 'person_birthplace', 
                                '_confidence_birthplace', 'person_dateofbirth', '_confidence_dateofbirth', 'person_race', 
                                '_confidence_race', 'person_religion', '_confidence_religion', 'person_sexualorientation', 
@@ -22,7 +22,7 @@ oscar_demographics.set_headers('movie_id', 'award_winner', '_', 'movie_rating', 
                                'person_bio', '_birthplace_gold', '_dateofbirth_gold', 'movie_name', 'person_name', '_r', '_re', 
                                '_s', '_y' )
 # Dropped race_ethnicity_gold, sexual_orientation_gold, religion_gold, year_of_award_gold, birthplace_gold, date_of_birth_gold and all confidence columns
-oscar_award = DataWrapper(Path('Consultant\Datasets\\the_oscar_award.csv'))
+oscar_award = DataWrapper(Path('data\\the_oscar_award.csv'))
 oscar_award.set_headers('movie_date', 'award_year', 'award_ceremony_number', 'award_category', 'person_name', 'movie_name', 
                         'award_winner')
 
