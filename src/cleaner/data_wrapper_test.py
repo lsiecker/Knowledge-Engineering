aa2f8e4f4e730eb6ee2134cd3ad5539c68f79335
Luc Siecker <lsiecker@quicknet.nl>
2023-06-07 09:57:35 -0400

    Commented Datawrapper test

--- src/cleaner/data_wrapper_test.py
+++ src/cleaner/data_wrapper_test.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 from data_wrapper import DataWrapper, DataSet, DataMatcher
 
+# Making DataWrappers for all the datasets
+# Design choice: Drop all columns that are not used by prefixing a '_' to the column name
 IMDB_top_250 = DataWrapper(Path('data\IMDB Top 250 Movies.csv'), 'IMDB Top 250 Movies')
 IMDB_top_250.set_headers('_', 'movie_name', 'movie_date', 'movie_rating', 'movie_genre', 'movie_censor', 'movie_runtime', 
                          'movie_overview', 'movie_budget', 'movie_box_office', 'actors', 'directors', 'writers')
@@ -21,7 +23,6 @@ oscar_demographics.set_headers('movie_id', 'award_winner', '_', 'movie_rating', 
                                '_confidence_sexualorientation', 'award_year', '_confidence_award_year', 'award_category', 
                                'person_bio', '_birthplace_gold', '_dateofbirth_gold', 'movie_name', 'person_name', '_r', '_re', 
                                '_s', '_y' )
-# Dropped race_ethnicity_gold, sexual_orientation_gold, religion_gold, year_of_award_gold, birthplace_gold, date_of_birth_gold and all confidence columns
 oscar_award = DataWrapper(Path('data\\the_oscar_award.csv'), 'Oscar award')
 oscar_award.set_headers('movie_date', 'award_year', '_award_ceremony_number', 'award_category', 'award_person', 'movie_name', 
                         'award_winner')
@@ -31,9 +32,12 @@ movie_meta = DataWrapper(Path('data\movie.metadata.tsv'), 'Movie metadata')
 movie_meta.set_headers('_', '_', 'movie_name', 'movie_date', 'movie_revenue', 'movie_runtime', 'movie_language', 'movie_country', 'movie_genre')
 
 
-# Excluded character_meta since it is very large and takes 12 hours to run
+# Making a list of all the datasets
+# Design choice: Dropped the character metadata and movie metadata datasets, because they are big 
+# and increasing the runtime of the program by hours.
 datasets = [IMDB_top_250, IMDB_all_genres, movies, mymovies, oscar_demographics, oscar_award]
 
+# Making the cleaned datasets by using a DataSet object
 cleaned_movies  = DataSet('Movie')
 cleaned_movies.set_headers('movie_name', 'movie_date', 'movie_censor', 'movie_genre', 'movie_rating')
 cleaned_persons = DataSet('Person')
@@ -62,32 +66,36 @@ cleaned_won_person.set_headers('person_name', 'award_category', 'award_year', 'p
 cleaned_has_genre = DataSet('Has genre')
 cleaned_has_genre.set_headers('movie_name', 'movie_date', 'movie_genre',)
 
+# For each dataset, add data to the cleaned datasets, but only for the columns that are in the cleaned datasets
 for data in datasets:
-    # Cleaning of the datasets
+    # Making date columns datetime
     for col in ['movie_date', 'movie_rating_time', 'award_year']:
         if col in data.get_headers():
             # For the given columns, make it datetime
             data.make_date(col)
 
     # For each dataset, add data to the cleaned datasets, but only for the columns that are in the cleaned datasets
-    # For actors, writers, directors, we need to split the data into multiple rows
-
     for cleaned_data in [cleaned_movies, cleaned_persons, cleaned_awards, cleaned_genres, \
                          cleaned_acted_in, cleaned_directed, cleaned_wrote, \
                          cleaned_nominated_for, cleaned_won, cleaned_nominated_for_person, \
                          cleaned_won_person, cleaned_has_genre]:
-        # For the columns that needs to be in the cleaned dataset
-        # Copy the rows from the original dataset to the cleaned dataset for the specific columns
 
         # Get the columns that are in both datasets
         common_cols = list(set(data.get_headers()).intersection(cleaned_data.get_headers()))
 
         # Make a dataframe with the data from the common columns
         df = data.get_data()[common_cols]
+
         # Add the data to the cleaned dataset
         cleaned_data.add_data(df)
+
+        # For the data with multiple values in one cell, explode the data (e.g. multiple actors for one movie)
         cleaned_data.explode_data()
 
+# Use the DataMatcher to match the data from the different datasets
+# The data is matched on the specified columns in the dataset and the cleaned datasets are updated accordingly
+# The DataMatcher also drops the unknown values from the cleaned datasets for the specified columns
+# The DataMatcher also exports the cleaned datasets to csv files such that they can be used in the next step of the knowledge graph.
 datamatcher = DataMatcher()
 cleaned_movies.update_data(datamatcher.aggregate(cleaned_movies.get_data(), "movie_name", "movie_date", dif_timestamps=True))
 cleaned_movies.drop_unknown('movie_name', "movie_date")
