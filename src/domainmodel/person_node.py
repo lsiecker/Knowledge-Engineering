194828d91aa8ec633558d049081690a6f6352725
Sander Moonemans <sander.moonemans@gmail.com>
2023-06-15 20:00:22 +0200

    changed naming of person fields and natural keys

--- src/domainmodel/person_node.py
+++ src/domainmodel/person_node.py
@@ -9,15 +9,14 @@ class Person(Node):
     start_year: int
     end_year: int
 
-    def __init__(self, name: str, dob: int, dod: int, start_year: int, end_year: int):
+    def __init__(self, name: str, date_of_birth: int, date_of_death: int, start_year: int, end_year: int):
         self.id = None
         self.label = 'Person'
         self.name = name
-        self.dob = dob
-        self.dod = dod
+        self.date_of_birth = date_of_birth
+        self.date_of_death = date_of_death
         self.start_year = start_year
         self.end_year = end_year
 
     def natural_keys(self) -> dict:
-        return {'name': self.name, 'year of birth': self.dob, 'year of death': self.dod, 
-                'activity started': self.start_year, 'activity ended': self.end_year}
+        return {'name': self.name, 'date_of_birth': self.date_of_birth}
