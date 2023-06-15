136d70917f6004699bcbba77b9a314bdac1d28c8
LukavdBoogaard <l.v.d.boogaard@student.tue.nl>
2023-06-15 18:43:51 +0200

    Added new relationship and updated error messages

--- src/domainmodel/person_node.py
+++ src/domainmodel/person_node.py
@@ -4,11 +4,13 @@ from src.domainmodel.neo4j_node import Node
 @dataclass
 class Person(Node):
     name: str
+    dob: int
 
-    def __init__(self, name: str):
+    def __init__(self, name: str, dob: int):
         self.id = None
         self.label = 'Person'
         self.name = name
+        self.dob = dob
 
     def natural_keys(self) -> dict:
-        return {'name': self.name}
+        return {'name': self.name, 'date of birth': self.dob}
