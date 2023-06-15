800820df655c8bb9f0cdf981e9046e94d2c055b8
LukavdBoogaard <l.v.d.boogaard@student.tue.nl>
2023-06-15 19:22:22 +0200

    created the relationships for in the db

--- src/domainmodel/person_node.py
+++ src/domainmodel/person_node.py
@@ -5,12 +5,19 @@ from src.domainmodel.neo4j_node import Node
 class Person(Node):
     name: str
     dob: int
+    dod: int
+    start_year: int
+    end_year: int
 
-    def __init__(self, name: str, dob: int):
+    def __init__(self, name: str, dob: int, dod: int, start_year: int, end_year: int):
         self.id = None
         self.label = 'Person'
         self.name = name
         self.dob = dob
+        self.dod = dod
+        self.start_year = start_year
+        self.end_year = end_year
 
     def natural_keys(self) -> dict:
-        return {'name': self.name, 'date of birth': self.dob}
+        return {'name': self.name, 'year of birth': self.dob, 'year of death': self.dod, 
+                'activity started': self.start_year, 'activity ended': self.end_year}
--- src/population/neo4j_example.py
+++ src/population/neo4j_example.py
@@ -4,6 +4,8 @@ from src.domainmodel.person_node import Person
 from src.domainmodel.oscar_node import Oscar
 from src.domainmodel.genre_node import Genre
 from src.domainmodel.actedin_relation import ActedInRelation
+from src.domainmodel.directed_relation import DirectedRelation
+from src.domainmodel.wrote_relation import WroteRelation
 from src.domainmodel.movieNominated_relation import MovieNominatedForRelation
 from src.domainmodel.movieWon_relation import MovieHasWonRelation
 from src.domainmodel.hasGenre_relation import HasGenreRelation
@@ -11,9 +13,9 @@ import pandas as pd
 
 if __name__ == "__main__":
 
+    # Create the movie nodes 
     movies_df = pd.read_csv('data/cleaned_data/merged_movies.csv')
     for index, row in movies_df.iterrows():
-        # Create a movie
         title = row['movie_name']
         year = row['movie_date']
         rating = row['movie_rating']
@@ -23,19 +25,31 @@ if __name__ == "__main__":
         movie = Movie(title, year, rating, budget)
         movie.create()
 
+    # Create the Oscar nodes 
     oscar_df = pd.read_csv('data/cleaned_data/Award.csv')
     for index, row in oscar_df.iterrows():
-        # Create an Oscar
         category = row['award_category']
         year = row['award_year']
         oscar = Oscar(category, year)
         oscar.create()
 
+    # Create the genre nodes
     genre_df = pd.read_csv('data/cleaned_data/Genre.csv')
     for index, row in genre_df.iterrows():
         genre = row['movie_genre']
         genre = Genre(genre)
         genre.create()
+
+    # Create the person nodes 
+    person_df = pd.read_csv('data/cleaned_data/Person_extended.csv')
+    for index, row in person_df.iterrows():
+        name = row['person_name']
+        dob = row['birth_date']
+        dod = row['death_date']
+        start_year = row["start_activity"]
+        end_year = row['end_activity']
+        person = Person(name, dob, dod, start_year, end_year)
+        person.create()
 
     movieNominated_df = pd.read_csv('data/cleaned_data/Nominated for (Movie).csv')
     for index, row in movieNominated_df.iterrows():
@@ -84,9 +98,48 @@ if __name__ == "__main__":
         hasGenre = HasGenreRelation(movie, genre)
         hasGenre.create()
 
+    actedIn_df = pd.read_csv('data/cleaned_data/Acted in.csv')
+    for index, row in actedIn_df.iterrows():
+        # Create the movie 
+        movie_name = row['movie_name']
+        movie_date = row['movie_date']
+        movie = Movie(movie_name, movie_date, None, None)
+
+        # Create the actor 
+        actor = row['actor']
+        actor = Person(actor)
+
+        # Create the relationship
+        actedIn = ActedInRelation(actor, movie)
+        actedIn.create()
     
+    directed_df = pd.read_csv('data/cleaned_data/Directed.csv')
+    for index, row in directed_df.iterrows():
+        # Create the movie 
+        movie_name = row['movie_name']
+        movie_date = row['movie_date']
+        movie = Movie(movie_name, movie_date, None, None)
 
+        # Create the director 
+        director = row['director']
+        director = Person(director)
 
+        # Create the relationship
+        directed = DirectedRelation(director, movie)
+        directed.create()
 
+    wrote_df = pd.read_csv('data/cleaned_data/Wrote.csv')
+    for index, row in wrote_df.iterrows():
+        # Create the movie 
+        movie_name = row['movie_name']
+        movie_date = row['movie_date']
+        movie = Movie(movie_name, movie_date, None, None)
 
+        # Create the writer 
+        writer = row['writer']
+        writer = Person(writer)
+
+        # Create the relationship
+        writed = WroteRelation(director, movie)
+        writed.create()
 
